3cc21d1c339dbfe505be04dca9aebfdf215c14d6
taozhang1029 <taozhang1029@qq.com>
2022-08-27 23:43:11 +0800

    初体验

---
+++ dubbo/dubbo-interface/src/main/java/com/kingsley/study/dubbo/service/MemberInfoService.java
@@ -0,0 +1,12 @@
+package com.kingsley.study.dubbo.service;
+
+/**
+ * @author ZhangTao
+ * @date 2022/8/27
+ * @desc
+ */
+public interface MemberInfoService {
+
+    Boolean isMember(String userId);
+
+}
---
+++ dubbo/user-service-provider/src/main/java/com/kingsley/study/dubbo/service/impl/MemberInfoServiceImpl.java
@@ -0,0 +1,17 @@
+package com.kingsley.study.dubbo.service.impl;
+
+import com.kingsley.study.dubbo.service.MemberInfoService;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author ZhangTao
+ * @date 2022/8/27
+ * @desc
+ */
+@Component
+public class MemberInfoServiceImpl implements MemberInfoService {
+
+    public Boolean isMember(String userId) {
+        return null;
+    }
+}
