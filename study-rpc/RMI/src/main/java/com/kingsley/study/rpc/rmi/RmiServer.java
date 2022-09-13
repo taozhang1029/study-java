28f045ae1de1b37dc3a03d183c562a9b29e022ca
kingsley <taozhang1029@qq.com>
2022-09-14 01:21:10 +0800

    指定通信端口

--- study-rpc/RMI/src/main/java/com/kingsley/study/rpc/rmi/RmiServer.java
+++ study-rpc/RMI/src/main/java/com/kingsley/study/rpc/rmi/RmiServer.java
@@ -3,11 +3,15 @@ package com.kingsley.study.rpc.rmi;
 import com.kingsley.study.rpc.rmi.service.HelloService;
 import com.kingsley.study.rpc.rmi.service.impl.HelloServiceImpl;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.rmi.AlreadyBoundException;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
+import java.rmi.server.RMISocketFactory;
 
 /**
  * @author kingsley
@@ -16,9 +20,25 @@ import java.rmi.registry.LocateRegistry;
  */
 public class RmiServer {
 
-    public static void main(String[] args) throws RemoteException, AlreadyBoundException, MalformedURLException {
+    public static void main(String[] args) throws IOException, AlreadyBoundException {
         HelloService helloService = new HelloServiceImpl();
         LocateRegistry.createRegistry(8801);
+        // 指定通信端口，防止被防火墙拦截
+        RMISocketFactory.setSocketFactory(new RMISocketFactory() {
+            @Override
+            public Socket createSocket(String host, int port) throws IOException {
+                return new Socket(host, port);
+            }
+
+            @Override
+            public ServerSocket createServerSocket(int port) throws IOException {
+                if (port <= 0) {
+                    port = 8501;
+                }
+                System.out.println("RMI port: " + port);
+                return new ServerSocket(port);
+            }
+        });
         Naming.bind("rmi://localhost:8801/helloService", helloService);
         System.out.println("RmiServer start up");
     }
